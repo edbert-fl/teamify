232d8210fec4ef2b89174d545a3231b30061d6d3
Edbert Felix Lim <edbert.fl@gmail.com>
2024-01-22 13:22:40 +0700

    Feature: Added API call for creating new user accounts

--- backend/elephantsql.js
+++ backend/elephantsql.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bcrypt = require("bcryptjs");
 var axios = require("axios");
 var cors = require("cors");
 var http = require("http");
@@ -42,21 +43,29 @@ app.get("/api/time", async function (req, res) {
   }
 });
 
-app.post("/api/add-organization", async function (req, res) {
+/* 
+Add a new organization to the database
+*/
+app.post("/organization/add", async function (req, res) {
   const { code, name } = req.body; // Extract organization name and code from the request body
 
   try {
     client = await pool.connect();
 
     const result = await client.query(
-      'INSERT INTO organization (organization_code, organization_name) VALUES ($1, $2) RETURNING *',
+      "INSERT INTO organization (organization_code, organization_name) VALUES ($1, $2) RETURNING *",
       [code, name]
     );
 
-    res.json({ organization: result.rows[0], message: "Organization added successfully!" });
+    res.json({
+      organization: result.rows[0],
+      message: "Organization added successfully!",
+    });
   } catch (error) {
     console.error("Error adding organization", error);
-    res.status(500).json({ error: "Internal Server Error", details: error.message });
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
   } finally {
     if (client) {
       client.release();
@@ -64,6 +73,75 @@ app.post("/api/add-organization", async function (req, res) {
   }
 });
 
+/* 
+Log into existing organization
+*/
+app.post("/organization/login", async function (req, res) {
+  const { organizationCode } = req.body;
+
+  try {
+    client = await pool.connect();
+
+    const result = await client.query(
+      "SELECT * FROM organization WHERE code = $1",
+      [organizationCode]
+    );
+
+    res.json({
+      organization: result.rows[0],
+      message: "Logged into organization successfully!",
+    });
+  } catch (error) {
+    console.error("Error logging into organization", error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  } finally {
+    if (client) {
+      client.release();
+    }
+  }
+});
+
+/* 
+Register a new user
+*/
+app.post("/user/register", async function (req, res) {
+  const { displayName, email, password, currOrganization } = req.body;
+
+  try {
+    client = await pool.connect();
+
+    const generatedSalt = await bcrypt.genSalt(16);
+    const hashedPassword = await bcrypt.hash(password, generatedSalt);
+
+    console.log("displayName:", displayName);
+    console.log("email:", email);
+    console.log("password:", password);
+    console.log("currOrganization:", currOrganization);
+
+    const result = await client.query(
+      "INSERT INTO users (username, email, hashed_password, salt, organization_code) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [displayName, email, hashedPassword, generatedSalt, currOrganization.code]
+    );
+
+    console.log("Resulting Data:", result.data);
+
+    res.json({
+      organization: result.rows[0],
+      message: "Organization added successfully!",
+    });
+  } catch (error) {
+    console.error("Error adding organization", error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  } finally {
+    if (client) {
+      client.release();
+    }
+  }
+});
 
 const PORT = 3000;
 
