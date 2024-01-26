3d7cbf6ba538e6d82c8a657055c314f3d2dbd9bf
Edbert Felix Lim <edbert.fl@gmail.com>
2024-01-26 16:00:04 +0700

    Feature: Added bottom tab navigator

--- app/components/AuthAppNavigator.tsx
+++ app/components/AuthAppNavigator.tsx
@@ -1,14 +1,69 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ClockInScreen from "../screens/ClockInScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { theme } from "../utils/Styles";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { RootBottomTabParamList } from "../utils/Types";
 
 const Root = () => {
+  const navigation =
+    useNavigation<BottomTabNavigationProp<RootBottomTabParamList>>();
   const Tab = createBottomTabNavigator();
+
+  const tabBarIcon = (focused, route) => {
+    const iconMappings = {
+      Home: {
+        name: "home",
+        color: focused ? theme.colors.primary : theme.colors.placeholderText,
+        size: 32,
+      },
+      ClockIn: {
+        name: "access-time",
+        color: focused ? theme.colors.primary : theme.colors.placeholderText,
+        size: 32,
+      },
+      Calendar: {
+        name: "calendar-today",
+        color: focused ? theme.colors.primary : theme.colors.placeholderText,
+        size: 28,
+      },
+      // Add more route name to icon mappings as needed
+    };
+
+    const { name, color: iconColor, size } = iconMappings[route.name] || {};
+
+    return <Icon name={name} color={iconColor} size={size} />;
+  };
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => tabBarIcon(focused, route),
+        tabBarStyle: {
+          backgroundColor: theme.colors.tabBackground,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 100,
+          paddingTop: 5,
+          borderTopWidth: 0
+        },
+        tabBarLabelStyle: {
+          color: theme.colors.placeholderText,
+          fontSize: 12,
+          fontWeight: 500
+        }
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
@@ -17,6 +72,11 @@ const Root = () => {
       <Tab.Screen
         name="ClockIn"
         component={ClockInScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={HomeScreen}
         options={{ headerShown: false }}
       />
     </Tab.Navigator>
