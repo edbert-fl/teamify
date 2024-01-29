fc3dd990a31c5d2e94052c1ff72d7c0cb1650ac4
Edbert Felix Lim <edbert.fl@gmail.com>
2024-01-29 18:17:13 +0700

    Feature: Added AsyncStorage for storing user credentials

--- App.js
+++ App.js
@@ -1,13 +1,81 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import React from "react";
 import AppNavigator from "./app/components/AppNavigator";
 import { AppContext } from "./app/components/AppContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [currUser, setCurrUser] = useState(null);
   const [currOrganization, setCurrOrganization] = useState(null);
+
+  // Everytime app is loaded attempt to retrieve the userSession.
+  useEffect(() => {
+    if (currUser === null || currOrganization === null) {
+      retrieveUserSession();
+    }
+  }, []);
+
+  useEffect(() => {
+    storeUserSession();
+    console.log(currUser);
+  }, [currUser]);
+
+  useEffect(() => {
+    storeOrganizationSession();
+    console.log(currOrganization);
+  }, [currOrganization]);
+
+  async function storeUserSession() {
+    try {
+      await AsyncStorage.setItem(
+        "sessionUserInformation",
+        JSON.stringify(currUser)
+      );
+    } catch (error) {
+      console.log("Error storing user information: ", error);
+    }
+  }
+
+  async function storeOrganizationSession() {
+    try {
+      await AsyncStorage.setItem(
+        "sessionOrganizationInformation",
+        JSON.stringify(currOrganization)
+      );
+    } catch (error) {
+      console.log("Error storing user information: ", error);
+    }
+  }
+
+  async function retrieveUserSession() {
+    try {
+      console.log(
+        "Retrieving user session information..."
+      )
+      const userJSON = await AsyncStorage.getItem(
+        "sessionUserInformation"
+      );
+      const organizationJSON = await AsyncStorage.getItem(
+        "sessionOrganizationInformation"
+      );
+
+      if (userJSON !== null) {
+        const sessionUserInformation = JSON.parse(userJSON);
+        console.log("Session User Info:", sessionUserInformation);
+        setCurrUser(sessionUserInformation);
+      }
+
+      if (organizationJSON !== null) {
+        const sessionOrganizationInformation = JSON.parse(organizationJSON);
+        console.log("Session Org Info: ", sessionOrganizationInformation);
+        setCurrOrganization(sessionOrganizationInformation);
+      }
+    } catch (error) {
+      console.log("Error retrieving user information: ", error);
+    }
+  }
 
   return (
     <NavigationContainer>
