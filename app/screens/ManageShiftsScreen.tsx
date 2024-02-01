79f2d2665fac8a36a41b6ea7be43e11b9b6ad095
Edbert Felix Lim <edbert.fl@gmail.com>
2024-02-01 13:05:54 +0700

    Feature: Added header to Manage Shifts page

--- app/screens/ManageShiftsScreen.tsx
+++ app/screens/ManageShiftsScreen.tsx
@@ -1,16 +1,32 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
-import { theme } from '../utils/Styles'
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import React from "react";
+import { theme } from "../utils/Styles";
+import AppHeader from "../components/AppHeader";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { AdminStackParamList } from "../utils/Types";
 
 const ManageShiftsScreen = () => {
+  const navigation = useNavigation<StackNavigationProp<AdminStackParamList>>();
+
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <View style={styles.container}>
-        
-      </View>
-    </SafeAreaView>
-  )
-}
+    <View style={styles.container}>
+      <AppHeader
+        title="Manage Shifts"
+        onBackIcon={
+          <Icon
+            name="arrow-back-ios"
+            size={20}
+            color={theme.colors.primaryText}
+          />
+        }
+        onBackPress={() => navigation.goBack()}
+      />
+      <SafeAreaView style={styles.safeAreaView}></SafeAreaView>
+    </View>
+  );
+};
 
 export const styles = StyleSheet.create({
   safeAreaView: {
@@ -19,7 +35,6 @@ export const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingHorizontal: 20,
     paddingBottom: 50,
     backgroundColor: theme.colors.background,
   },
