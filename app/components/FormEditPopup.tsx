83ebd0d305bd49d28aa9e004f845ec750e207433
Edbert Felix Lim <edbert.fl@gmail.com>
2024-02-10 09:50:18 +0700

    Feature: Added animation to FormEditPopup

---
+++ app/components/FormEditPopup.tsx
@@ -0,0 +1,230 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Animated,
+} from "react-native";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Card } from "@rneui/themed";
+import { theme } from "../utils/Styles";
+
+const WINDOW_INNER_WIDTH = Dimensions.get("window").width;
+
+interface FormEditPopupProps {
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  valueLabel: string;
+  toggleFormEdit: () => void;
+}
+
+const FormEditPopup: React.FC<FormEditPopupProps> = ({
+  value,
+  setValue,
+  valueLabel,
+  toggleFormEdit,
+}) => {
+  const backgroundFadeAnim = useRef(new Animated.Value(0)).current;
+  const formFadeAnim = useRef(new Animated.Value(0)).current;
+  const formZoomAnim = useRef(new Animated.Value(0.8)).current;
+  const [tempValue, setTempValue] = useState<string>("");
+  const ANIMATION_DURATION = 100;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(backgroundFadeAnim, {
+        toValue: 0.8,
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(formFadeAnim, {
+        toValue: 1,
+        duration: ANIMATION_DURATION * 2,
+        useNativeDriver: true,
+      }),
+      Animated.timing(formZoomAnim, {
+        toValue: 1,
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  const exitScreen = () => {
+    Animated.parallel([
+      Animated.timing(backgroundFadeAnim, {
+        toValue: 0,
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(formFadeAnim, {
+        toValue: 0,
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(formZoomAnim, {
+        toValue: 0.8,
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      setTimeout(() => {
+        toggleFormEdit();
+      }, ANIMATION_DURATION);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          ...styles.overlay,
+          opacity: backgroundFadeAnim,
+        }}
+      ></Animated.View>
+      <Animated.View
+        style={{
+          width: "100%",
+          position: "absolute",
+          alignSelf: "center",
+          alignItems: "center",
+          top: "20%",
+          opacity: formFadeAnim,
+          transform: [{ scale: formZoomAnim }],
+        }}
+      >
+        <Card containerStyle={styles.card}>
+          <View style={{ width: 300 }}>
+            <View style={styles.cardTitleContainer}>
+              <Text style={styles.cardTitle}>Edit {valueLabel}</Text>
+            </View>
+            <View>
+              <Text style={styles.inputLabel}>{valueLabel.toUpperCase()}</Text>
+              <TextInput
+                value={tempValue}
+                style={styles.input}
+                placeholder={value}
+                placeholderTextColor={theme.colors.primaryText}
+                autoCapitalize="none"
+                keyboardType="numeric"
+                onChangeText={(text) => setTempValue(text)}
+                enablesReturnKeyAutomatically
+              />
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.cancelButton}
+                  onPress={exitScreen}
+                >
+                  <Text style={styles.buttonText}>CANCEL</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.doneButton}
+                  onPress={exitScreen}
+                >
+                  <Text style={styles.buttonText}>DONE</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Card>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    height: "100%",
+    width: WINDOW_INNER_WIDTH,
+  },
+  overlay: {
+    backgroundColor: "black",
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 30,
+  },
+  cancelButton: {
+    backgroundColor: theme.colors.accent,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    alignItems: "center",
+    width: 80,
+  },
+  doneButton: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    width: 80,
+  },
+  buttonText: {
+    color: theme.colors.primaryText,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: theme.colors.surface,
+    text: theme.colors.surface,
+    borderWidth: 0,
+    borderRadius: 8,
+    paddingVertical: 25,
+    paddingHorizontal: 0,
+    width: "90%",
+    position: "absolute",
+    alignSelf: "center",
+    alignItems: "center",
+    top: "20%",
+  },
+  cardTitle: {
+    color: theme.colors.primaryText,
+    fontWeight: "700",
+    textAlign: "center",
+    fontSize: 24,
+  },
+  cardTitleContainer: {
+    width: "100%",
+    marginBottom: 20,
+  },
+  tableRow: {
+    flexDirection: "row",
+    paddingVertical: 6,
+  },
+  labelCell: {
+    flex: 3,
+    textAlign: "left",
+    marginBottom: 3,
+  },
+  inputLabel: {
+    color: theme.colors.primaryText,
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 8,
+    marginLeft: 6,
+  },
+  input: {
+    color: theme.colors.secondaryText,
+    fontSize: 16,
+    backgroundColor: theme.colors.background,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 6,
+    width: "100%",
+  },
+});
+
+export default FormEditPopup;
--- app/screens/EditUserScreen.tsx
+++ app/screens/EditUserScreen.tsx
@@ -1,49 +1,174 @@
-import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet, Platform, StatusBar} from 'react-native'
-import React from 'react'
-import { theme } from '../utils/Styles';
-import { AdminStackRouteProp } from '../utils/Types';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons"; // Importing an icon library
+import { theme } from "../utils/Styles";
+import {
+  AdminStackParamList,
+  AdminStackRouteProp,
+  userRoles,
+} from "../utils/Types";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import AppHeader from "../components/AppHeader";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import FormEditPopup from "../components/FormEditPopup";
 
 interface EditUserScreenProps {
-    route: AdminStackRouteProp<'EditUser'>;
+  route: AdminStackRouteProp<"EditUser">;
 }
 
 const EditUserScreen: React.FC<EditUserScreenProps> = ({ route }) => {
-    const { userToEdit } = route.params
-    
-    return (
-        <SafeAreaView style={styles.safeAreaView}>
-          <View style={styles.container}>
-            <Text>{userToEdit.username}</Text>
+  const { userToEdit } = route.params;
+  const [rateFormOpen, setRateFormOpen] = useState(false);
+  const [rate, setRate] = useState<string>("");
+  const navigation = useNavigation<StackNavigationProp<AdminStackParamList>>();
+
+  const toggleRateFormOpen = () => {
+    setRateFormOpen(!rateFormOpen);
+  };
+
+  return (
+    <View style={styles.container}>
+      <AppHeader
+        title={userToEdit.username}
+        onBackIcon={
+          <Icon
+            name="arrow-back-ios"
+            size={20}
+            color={theme.colors.primaryText}
+          />
+        }
+        onBackPress={() => navigation.goBack()}
+      />
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.userInfoContainer}>
+          <View style={styles.fieldContainer}>
+            <View>
+              <Text style={styles.label}>EMAIL</Text>
+              <Text style={styles.value}>{userToEdit.email}</Text>
+            </View>
           </View>
-        </SafeAreaView>
-      );
-    };
-    
-    export const styles = StyleSheet.create({
-      safeAreaView: {
-        flex: 1,
-        backgroundColor: theme.colors.background,
-        paddingTop: Platform.OS === "android" ? (StatusBar.currentHeight as number) : 0
-      },
-      container: {
-        flex: 1,
-        paddingHorizontal: 20,
-        paddingBottom: 50,
-        backgroundColor: theme.colors.background,
-      },
-      button: {
-        backgroundColor: theme.colors.accent,
-        borderRadius: 5,
-        padding: 15,
-        marginBottom: 20,
-        alignItems: "center",
-      },
-      buttonText: {
-        color: "white",
-        fontSize: 16,
-        fontWeight: "bold",
-      },
-    });
-    
-    export default EditUserScreen;
-    
+
+          <View style={styles.fieldContainer}>
+            <View>
+              <Text style={styles.label}>RATE</Text>
+              <Text style={styles.value}>${userToEdit.rate}/hr</Text>
+            </View>
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={toggleRateFormOpen}
+            >
+              <Text style={styles.editButtonText}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.fieldContainer}>
+            <View>
+              <Text style={styles.label}>ROLE</Text>
+              <Text style={styles.value}>{userRoles[userToEdit.role_id]}</Text>
+            </View>
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={() => {
+                /* Add your edit functionality for Role */
+              }}
+            >
+              <Text style={styles.editButtonText}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            /* Add your edit functionality for the entire form */
+          }}
+        >
+          <Text style={styles.buttonText}>Edit User</Text>
+        </TouchableOpacity>
+
+        {rateFormOpen ? (
+          <>
+            <FormEditPopup
+              value={rate}
+              setValue={setRate}
+              valueLabel="Rate"
+              toggleFormEdit={toggleRateFormOpen}
+            />
+          </>
+        ) : null}
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    paddingTop:
+      Platform.OS === "android" ? (StatusBar.currentHeight as number) : 0,
+    paddingHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+    paddingBottom: 50,
+    backgroundColor: theme.colors.background,
+  },
+  userInfoContainer: {
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: theme.colors.text,
+  },
+  value: {
+    fontSize: 18,
+    marginBottom: 15,
+    color: theme.colors.secondaryText,
+  },
+  button: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    padding: 15,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  fieldContainer: {
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  editButton: {
+    backgroundColor: theme.colors.accent,
+    borderRadius: 5,
+    padding: 10,
+    paddingHorizontal: 20,
+    marginLeft: 10,
+    alignItems: "center",
+  },
+  editButtonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
+
+export default EditUserScreen;
